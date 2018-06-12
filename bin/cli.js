494f9d08c61e9a467ebec4d0f5ffe7cb9ffa964a
Mark Birbeck <mark.birbeck@gmail.com>
2018-06-12 17:35:13 +0100

    [#3] Wrap code in main() so that we can add async/await

--- bin/cli.js
+++ bin/cli.js
@@ -1,128 +1,132 @@
 #!/usr/bin/env node
 'use strict';
-const path = require('path');
-require('dotenv').config((process.env.DOT_ENV_FILE) ? {path: process.env.DOT_ENV_FILE} : undefined);
+const main = () => {
+  const path = require('path');
+  require('dotenv').config((process.env.DOT_ENV_FILE) ? {path: process.env.DOT_ENV_FILE} : undefined);
 
-const Pipeline = require('sifttt/lib/beam/Pipeline');
-const Read = require('sifttt/lib/beam/Read');
-const Write = require('sifttt/lib/beam/Write');
-const ct = require('sifttt/lib/beam/coreTransforms');
+  const Pipeline = require('sifttt/lib/beam/Pipeline');
+  const Read = require('sifttt/lib/beam/Read');
+  const Write = require('sifttt/lib/beam/Write');
+  const ct = require('sifttt/lib/beam/coreTransforms');
 
-const yargs = require('yargs');
+  const yargs = require('yargs');
 
-let argv = yargs
-.usage('$0 command')
-.version()
-.env()
-.config()
-.global(['config'])
-.commandDir(path.join(__dirname, '../commands'))
-.demand(1, 'must provide a valid command')
-.help('h')
-.alias('h', 'help')
-.argv;
+  let argv = yargs
+  .usage('$0 command')
+  .version()
+  .env()
+  .config()
+  .global(['config'])
+  .commandDir(path.join(__dirname, '../commands'))
+  .demand(1, 'must provide a valid command')
+  .help('h')
+  .alias('h', 'help')
+  .argv;
 
-const command = argv._[0];
+  const command = argv._[0];
 
-let sourceModulePath;
-let sinkModulePath;
+  let sourceModulePath;
+  let sinkModulePath;
 
-if (command === 'source') {
-  sourceModulePath = argv.modulePath;
-} else {
-  sourceModulePath = 'sifttt/lib/beam/StdinSource';
-}
-
-if (command === 'sink') {
-  sinkModulePath = argv.modulePath;
-} else {
-  sinkModulePath = 'sifttt/lib/beam/StdoutSink';
-}
-
-let Source;
-try {
-  if (argv.verbose) {
-    console.error('About to load source from:', sourceModulePath);
-  }
-  Source = require(sourceModulePath);
-} catch (e) {
-  if (e instanceof Error && e.code === 'MODULE_NOT_FOUND') {
-      console.error(`Can't load module '${sourceModulePath}'. Please ensure it is installed.`);
-      process.exit(-1);
+  if (command === 'source') {
+    sourceModulePath = argv.modulePath;
   } else {
-    throw e;
+    sourceModulePath = 'sifttt/lib/beam/StdinSource';
   }
-}
 
-let Sink;
-try {
-  if (argv.verbose) {
-    console.error('About to load sink from:', sinkModulePath);
-  }
-  Sink = require(sinkModulePath);
-} catch (e) {
-  if (e instanceof Error && e.code === 'MODULE_NOT_FOUND') {
-      console.error(`Can't load module '${sinkModulePath}'. Please ensure it is installed.`);
-      process.exit(-1);
+  if (command === 'sink') {
+    sinkModulePath = argv.modulePath;
   } else {
-    throw e;
+    sinkModulePath = 'sifttt/lib/beam/StdoutSink';
   }
-}
 
-/**
- * Create an instance of our source, and if it's not a pipeline than wrap it
- * in a Read.from():
- */
-
-let source = new Source(argv);
-
-if (source.getStream) {
-  source = Read.from(source);
-}
-
-let pipelineIn = Pipeline.create({name: command})
-.apply(source);
-
-let pipelineTransform = pipelineIn;
-
-if (ct.hasOwnProperty(command)) {
-  let Module;
-
+  let Source;
   try {
-    Module = require(argv.modulePath);
+    if (argv.verbose) {
+      console.error('About to load source from:', sourceModulePath);
+    }
+    Source = require(sourceModulePath);
   } catch (e) {
     if (e instanceof Error && e.code === 'MODULE_NOT_FOUND') {
-        console.error(`Can't load module '${argv.modulePath}'. Please ensure it is installed.`);
+        console.error(`Can't load module '${sourceModulePath}'. Please ensure it is installed.`);
         process.exit(-1);
     } else {
       throw e;
     }
   }
 
-  pipelineTransform = pipelineTransform
-  .apply(new ct.ParseJSON())
-  .apply(new ct[command](Module(argv)));
-}
-
-/**
- * Create an instance of our sink, and if it's not a pipeline than wrap it
- * in a Write.to():
- */
-
-let sink = new Sink(argv);
-
-if (sink.getStream) {
-  sink = Write.to(sink);
-}
-
-let pipelineOut = pipelineTransform
-.apply(new ct.ToVinyl())
-.apply(sink)
-;
-
-pipelineOut.run(() => {
-  if (argv.verbose) {
-    console.error(`Finished Processing: ${command}`);
+  let Sink;
+  try {
+    if (argv.verbose) {
+      console.error('About to load sink from:', sinkModulePath);
+    }
+    Sink = require(sinkModulePath);
+  } catch (e) {
+    if (e instanceof Error && e.code === 'MODULE_NOT_FOUND') {
+        console.error(`Can't load module '${sinkModulePath}'. Please ensure it is installed.`);
+        process.exit(-1);
+    } else {
+      throw e;
+    }
   }
-});
-;
+
+  /**
+   * Create an instance of our source, and if it's not a pipeline than wrap it
+   * in a Read.from():
+   */
+
+  let source = new Source(argv);
+
+  if (source.getStream) {
+    source = Read.from(source);
+  }
+
+  let pipelineIn = Pipeline.create({name: command})
+  .apply(source);
+
+  let pipelineTransform = pipelineIn;
+
+  if (ct.hasOwnProperty(command)) {
+    let Module;
+
+    try {
+      Module = require(argv.modulePath);
+    } catch (e) {
+      if (e instanceof Error && e.code === 'MODULE_NOT_FOUND') {
+          console.error(`Can't load module '${argv.modulePath}'. Please ensure it is installed.`);
+          process.exit(-1);
+      } else {
+        throw e;
+      }
+    }
+
+    pipelineTransform = pipelineTransform
+    .apply(new ct.ParseJSON())
+    .apply(new ct[command](Module(argv)));
+  }
+
+  /**
+   * Create an instance of our sink, and if it's not a pipeline than wrap it
+   * in a Write.to():
+   */
+
+  let sink = new Sink(argv);
+
+  if (sink.getStream) {
+    sink = Write.to(sink);
+  }
+
+  let pipelineOut = pipelineTransform
+  .apply(new ct.ToVinyl())
+  .apply(sink)
+  ;
+
+  pipelineOut.run(() => {
+    if (argv.verbose) {
+      console.error(`Finished Processing: ${command}`);
+    }
+  });
+  ;
+}
+
+main()
